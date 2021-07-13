8998fc46f35b5c84cb2c15fb52a17e45429798dc
Jonas <github@barrskog.com>
2021-07-13 21:32:55 +0200

    Adding support for Mac OS

---
+++ Brewfile
@@ -0,0 +1,52 @@
+# taps
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+
+brew "mas"
+brew "svn"  # Needed for source-code-pro (not *-nerd-font)
+
+# Applications
+cask "kitty"
+cask "hazel"
+cask "istat-menus"
+cask "obsidian"
+cask "alfred"
+cask "bettertouchtool"
+
+# Fonts
+cask "font-fira-code"
+cask "font-jetbrains-mono"
+cask "font-cascadia-mono"
+cask "font-3270-nerd-font"
+cask "font-source-code-pro"
+cask "font-sauce-code-pro-nerd-font"
+
+# macOS App Store
+mas "Fantastical", id: 975937182
+mas "Xcode", id: 497799835
+mas "Deliveries", id: 290986013
+mas "1Password", id: 1333542190
+mas "Timery", id: 1425368544
+
+tap "homebrew/bundle"
+tap "homebrew/core"
+
+# packages
+brew "ack"
+brew "bat"
+brew "cloc"
+brew "git-delta"
+brew "exa"
+brew "fzf"
+brew "git"
+brew "gnupg"
+brew "grep"
+brew "htop"
+brew "lazygit"
+brew "markdown"
+brew "neovim"
+brew "python"
+brew "shellcheck"
+brew "tmux"
+brew "z"
+brew "zsh"
---
+++ osx_setup.sh
@@ -0,0 +1,199 @@
+#!/usr/bin/env bash
+
+DOTFILES="$(pwd)"
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+error() {
+    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+    exit 1
+}
+
+warning() {
+    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+info() {
+    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+}
+
+success() {
+    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+setup_git() {
+    title "Setting up Git"
+
+    defaultName=$(git config user.name)
+    defaultEmail=$(git config user.email)
+    defaultGithub=$(git config github.user)
+
+    read -rp "Name [$defaultName] " name
+    read -rp "Email [$defaultEmail] " email
+    read -rp "Github username [$defaultGithub] " github
+
+    git config -f ~/.gitconfig-local user.name "${name:-$defaultName}"
+    git config -f ~/.gitconfig-local user.email "${email:-$defaultEmail}"
+    git config -f ~/.gitconfig-local github.user "${github:-$defaultGithub}"
+
+    if [[ "$(uname)" == "Darwin" ]]; then
+        git config --global credential.helper "osxkeychain"
+    else
+        read -rn 1 -p "Save user and password to an unencrypted file to avoid writing? [y/N] " save
+        if [[ $save =~ ^([Yy])$ ]]; then
+            git config --global credential.helper "store"
+        else
+            git config --global credential.helper "cache --timeout 3600"
+        fi
+    fi
+}
+
+setup_homebrew() {
+    title "Setting up Homebrew"
+
+    if test ! "$(command -v brew)"; then
+        info "Homebrew not installed. Installing."
+        # Run as a login shell (non-interactive) so that the script doesn't pause for user input
+        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+    fi
+
+    # install brew dependencies from Brewfile
+    brew bundle
+
+    # install fzf
+    echo -e
+    info "Installing fzf"
+    "$(brew --prefix)"/opt/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
+}
+
+function setup_shell() {
+    title "Configuring shell"
+
+    [[ -n "$(command -v brew)" ]] && zsh_path="$(brew --prefix)/bin/zsh" || zsh_path="$(which zsh)"
+    if ! grep "$zsh_path" /etc/shells; then
+        info "adding $zsh_path to /etc/shells"
+        echo "$zsh_path" | sudo tee -a /etc/shells
+    fi
+
+    if [[ "$SHELL" != "$zsh_path" ]]; then
+        chsh -s "$zsh_path"
+        info "default shell changed to $zsh_path"
+    fi
+}
+
+function setup_shell() {
+    title "Configuring shell"
+
+    [[ -n "$(command -v brew)" ]] && zsh_path="$(brew --prefix)/bin/zsh" || zsh_path="$(which zsh)"
+    if ! grep "$zsh_path" /etc/shells; then
+        info "adding $zsh_path to /etc/shells"
+        echo "$zsh_path" | sudo tee -a /etc/shells
+    fi
+
+    if [[ "$SHELL" != "$zsh_path" ]]; then
+        chsh -s "$zsh_path"
+        info "default shell changed to $zsh_path"
+    fi
+}
+
+setup_macos() {
+    title "Configuring macOS"
+
+        echo "Finder: show all filename extensions"
+        defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+        echo "show hidden files by default"
+        defaults write com.apple.Finder AppleShowAllFiles -bool false
+
+        echo "only use UTF-8 in Terminal.app"
+        defaults write com.apple.terminal StringEncodings -array 4
+
+        echo "expand save dialog by default"
+        defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+        echo "show the ~/Library folder in Finder"
+        chflags nohidden ~/Library
+
+        echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+        defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+        echo "Enable subpixel font rendering on non-Apple LCDs"
+        defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+        echo "Use current directory as default search scope in Finder"
+        defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+        echo "Show Path bar in Finder"
+        defaults write com.apple.finder ShowPathbar -bool true
+
+        echo "Show Status bar in Finder"
+        defaults write com.apple.finder ShowStatusBar -bool true
+
+        echo "Disable press-and-hold for keys in favor of key repeat"
+        defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+        echo "Set a blazingly fast keyboard repeat rate"
+        defaults write NSGlobalDomain KeyRepeat -int 1
+
+        echo "Set a shorter Delay until key repeat"
+        defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+        echo "Enable tap to click (Trackpad)"
+        defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+        echo "Enable Safari’s debug menu"
+        defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+        echo "Kill affected applications"
+
+        for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
+}
+
+case "$1" in
+    backup)
+        backup
+        ;;
+    link)
+        setup_symlinks
+        ;;
+    git)
+        setup_git
+        ;;
+    homebrew)
+        setup_homebrew
+        ;;
+    shell)
+        setup_shell
+        ;;
+    terminfo)
+        setup_terminfo
+        ;;
+    macos)
+        setup_macos
+        ;;
+    all)
+        setup_symlinks
+        setup_terminfo
+        setup_homebrew
+        setup_shell
+        setup_git
+        setup_macos
+        ;;
+    *)
+        echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
+        exit 1
+        ;;
+esac
+
+echo -e
+success "Done."
